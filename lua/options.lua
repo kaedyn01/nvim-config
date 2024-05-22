236013f1c29388ab4d77e4e934effa106b5f1ee5
Kaedyn <kaedynbolin1@gmail.com>
2024-05-22 00:06:41 -0500

    Updated comments and added additional option

--- lua/options.lua
+++ lua/options.lua
@@ -1,5 +1,35 @@
+--[[
+This file specifies various vim options that are
+turned on, off, or given a specific value.
+
+The structure for specifying commands is as follows:
+	vim.opt.command = true/false/number
+
+For example:
+	vim.opt.number = true
+
+This would be equivalent to the following command
+in vim:
+	set number
+
+Another example would be:
+	vim.opt.tabstop = 4
+
+The equivalent command in vim would be:
+	set tabstop=4 
+
+A list of all the availabe commands can be found 
+at https://vimdoc.sourceforge.net/htmldoc/options.html
+--]]
+
 -- Show line numbers.
-vim.opt.number = true	
+vim.opt.number = true
+
+-- Show relative line numbers.
+vim.opt.relativenumber = true
+
+-- Put line at the 100th character.
+vim.opt.colorcolumn = "100"
 
 -- Defines length of autoindent in spaces. 
 vim.opt.shiftwidth = 4
