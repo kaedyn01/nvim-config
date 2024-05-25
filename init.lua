5bc9186939c6c243c5e3ecca2562c09b0649d474
Kaedyn <kaedynbolin1@gmail.com>
2024-05-24 19:49:22 -0500

    Made minimal template that loads Lazy package manager

--- init.lua
+++ init.lua
@@ -1,3 +1,8 @@
 print("Good morning")
 
+--[[
+Loads the Lazy package manager.
+Lazy is located in lua/config/ due to it not working
+when being placed in the parent directory (lua).
+--]]
 require("config.lazy")
