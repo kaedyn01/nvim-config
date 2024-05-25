e221d480580729b371369baa063b0ea78df5a5f6
Kaedyn <kaedynbolin1@gmail.com>
2024-05-24 19:39:22 -0500

    Got lazy package manager to work

--- init.lua
+++ init.lua
@@ -1,18 +1,3 @@
--- Print greeting message
-local current_hour = tonumber(os.date("%H"))
+print("Good morning")
 
-if current_hour >= 19 then
-	print("Good eveneing")
-elseif current_hour >= 12 then
-	print("Good afternoon")
-elseif current_hour >= 5 then
-	print("Good morning")
-else
-	print("Good evening")
-end
-
--- Load custom modules/settings
-require("core.options")
-
--- Load package manager
-require("lazy")
+require("config.lazy")
